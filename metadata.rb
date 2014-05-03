f56f9c1abfb8263e4e8b6e11680573e4496295c8
Ishtiaq Ahmed <ishtiaq@techish.com>
2014-05-03 10:01:51 +1000

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'was'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures was'
+long_description 'Installs/Configures was'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: was
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
